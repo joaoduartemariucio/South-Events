968b009bee4fa96c551355febf0e47347c76bca3
João Vitor Duarte Mariucio <joao.duarte@iconnectdobrasil.com.br>
2021-04-13 19:07:40 -0300

    Arquivos base tela inicial

---
+++ southsystem-desafio/View /TelaInicial/TelaInicialView.swift
@@ -0,0 +1,20 @@
+//
+//  TelaInicialView.swift
+//  southsystem-desafio
+//
+//  Created by João Vitor Duarte Mariucio on 13/04/21.
+//
+
+import UIKit
+
+class TelaInicialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
