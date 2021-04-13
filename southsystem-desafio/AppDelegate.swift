57a5af90106756a23f9282ff5c619f63309cb71e
João Vitor Duarte Mariucio <joao.duarte@iconnectdobrasil.com.br>
2021-04-13 18:48:10 -0300

    Inicio do versionamento

---
+++ southsystem-desafio/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  southsystem-desafio
+//
+//  Created by João Vitor Duarte Mariucio on 13/04/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
